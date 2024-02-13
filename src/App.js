35ce799ca7799d5695be89624d53504d90529659
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2024-02-13 13:35:54 +0100

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Pilar Dominguez</footer>
+      </div>
     </div>
   );
 }
