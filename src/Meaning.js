37e56c53d4485bfab1cc2b8e6cc9876870b1f618
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2024-02-17 19:37:22 +0100

    updated dictionary component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,3 +1,6 @@
 import React from "react";
 
-export default function Meaning(props) {}
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return <div className="Meaning"></div>;
+}
